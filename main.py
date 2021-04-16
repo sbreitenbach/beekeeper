01e2605815ca24d1f431aea9c33446c3b9fc796b
Scott Breitenbach <scott.breitenbach@gmail.com>
2021-04-15 21:11:46 -0400

    initial commit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import json
 import logging
+import requests
+from bs4 import BeautifulSoup
 
 ##Begin Config##
 logging.basicConfig(filename='log.log',
@@ -9,16 +11,36 @@ logging.basicConfig(filename='log.log',
                     level=logging.DEBUG)
 ##End Config##
 
-def returns_true():
-    return True
+
+def in_stock(soup):
+    buttons = soup.find_all(
+        'button', class_='btn product-form__cart-submit btn--secondary-accent')
+    if(buttons):
+        first_button = buttons[0]
+        button_string = str(first_button)
+        if "Sold out" in button_string:
+            print("out of stock")
+            return False
+        elif "Add to cart" in button_string:
+            print("in stock")
+            return True
+        else:
+            print("Not sure if in stock.")
+            logging.warn("Not sure if in stock.")
+            return False
+    else:
+        print("No buttons found")
+
 
 if __name__ == '__main__':
-    with open('publicConfig.json') as json_file:
+
+    with open('secretConfig.json') as json_file:
         data = json.load(json_file)
-        my_setting_agent = data["settings"]["1"]
+        my_proxies = data["proxies"]
+        my_user_agents = data["user_agents"]
 
+    result = requests.get("https://httpstat.us/418")
 
-    print("Hello World!")
-    print(returns_true())
-    print(my_setting_agent)
-    logging.info("Hello World!")
+    c = result.content
+    soup = BeautifulSoup(c)
+    in_stock(soup)
